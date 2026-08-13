5f70e08855a28b47f06f3cdd0f10dc7b9e2ba4ec
Hari Shanker Dhathrak <dhathrakharishankar@gmail.com>
2026-08-13 16:31:40 +0530

    done js

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,28 @@
-alert("sv");
+var users=[
+    {
+        "name":"john doe",
+        "gender":"male",
+        "image":"male.png"
+    },
+    {
+        "name":"jane doe",
+        "gender":"female",
+        "image":"female.png"
+    }
+]
+
+var curId=0;
+
+function toggleUser(){
+    curId=(curId+1)%2;
+
+    var username= document.getElementById("user-name").textContent=users[curId].name;
+    var userimage= document.getElementById("user-image");
+    var usergender= document.getElementById("user-gender");
+
+    // username.innerHTML=users[curId].name;
+    usergender.innerHTML=users[curId].gender;
+    userimage.src=users[curId].image;
+
+
+}
