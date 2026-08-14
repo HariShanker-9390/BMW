f805d582e833d21a0563c0b07679a327f0540982
Hari Shanker Dhathrak <dhathrakharishankar@gmail.com>
2026-08-14 10:36:19 +0530

    randomuser

--- frontend/index.js
+++ frontend/index.js
@@ -16,13 +16,38 @@ var curId=0;
 function toggleUser(){
     curId=(curId+1)%2;
 
-    var username= document.getElementById("user-name").textContent=users[curId].name;
+    var username= document.getElementById("user-name");
     var userimage= document.getElementById("user-image");
     var usergender= document.getElementById("user-gender");
 
-    // username.innerHTML=users[curId].name;
+    username.innerHTML=users[curId].name;
     usergender.innerHTML=users[curId].gender;
     userimage.src=users[curId].image;
+}
 
 
+function randomUser(){
+    fetch("https://randomuser.me/api")
+         .then(function(res){
+            return res.json();
+         })
+         .then(function(data){
+            var username= document.getElementById("user-name");
+            var userimage= document.getElementById("user-image");
+            var usergender= document.getElementById("user-gender"); 
+            var newUserName=data.results[0].name.first+" "+data.results[0].name.last;  
+            var newUserGender=data.results[0].gender;
+            var newUserImage=data.results[0].picture.large;
+
+            username.innerHTML=newUserName;
+            usergender.innerHTML=newUserGender;
+            userimage.src=newUserImage;
+
+            .catch(function(err){
+                console.log("Error occured:"+err);
+            }
+
+
+
+         })
 }
